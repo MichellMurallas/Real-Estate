c9b482f0c9223d3187495df7d617c76b88e7fa59
MichellMurallas <michellsms893@gmail.com>
2023-03-13 17:56:47 -0500

    hashing

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -17,8 +17,8 @@ const registrar = async(req, res)=>{
   //Validacion:
   await check("nombre").notEmpty().withMessage("El campo nombre es hobligatorio").run(req);
   await check("email").isEmail().withMessage("Asegurate de escribir bien el correo").run(req);
-  await check("password").isLength({min: 6 }).withMessage("debe contener minimo 6 caracteres").run(req);
-  await check("repetir_password").equals("password").withMessage("Los passwords no son iguales").run(req)
+  await check("password").isLength({min: 6 }).withMessage("El password debe contener minimo 6 caracteres").run(req);
+  await check("repetir_password").equals(req.body.password).withMessage("Los passwords no son iguales").run(req)
   
   let resultado = validationResult(req)
 
@@ -36,10 +36,31 @@ const registrar = async(req, res)=>{
     })
   }
 
+  //extraer los datos:
+  const { nombre, email, password } = req.body
 
-  const usuario = await Usuario.create(req.body)
+  //Verificar que el usuario no este duplicado:
+  const existeUsuario = await Usuario.findOne({where : {email}})
 
-  res.json(usuario)
+  if(existeUsuario){
+    return res.render("auth/registro",{
+      pagina: "Crear Cuenta",
+      errores: [{msg: "El usuario con este email ya esta registrado"}],
+      usuario: {
+      nombre: req.body.nombre,
+      email: req.body.email
+      }
+    })
+  }
+
+  //Almacenar usuario:
+  await Usuario.create({
+    nombre,
+    email,
+    password,
+    token: 123
+  })
+
 }
 
 const formularioOlvidePassword = (req, res)=>{
