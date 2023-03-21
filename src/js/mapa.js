1835ca7e3fb4175439a2beb1b86f9be5b196c2ee
MichellMurallas <michellsms893@gmail.com>
2023-03-21 14:12:39 -0500

    map1

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,1 +1,37 @@
-alert("Soy el mapa")
+(function() {
+  
+  const lat = 4.7212732;
+  const lng = -74.0410096;
+  const mapa = L.map('mapa').setView([lat, lng ], 15);
+  let marker;
+
+  //Utilizar Provider y Geocode
+  const geocodeService = L.Geovoding.geocodeService()
+  
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(mapa);
+
+
+  //Pin:
+
+  marker = new L.marker([lat, lng], {
+    draggable: true,
+    autoPan: true
+  })
+  .addTo(mapa)
+
+  //Detectar el movimiento del pin
+  marker.on("moveend", function(e){
+    marker = e.target
+    const posicion = marker.getLantLng();
+    mapa.panTo(new L.LatLng(posicion.lat, posicion.lng))
+
+    //Obtener la informacion de las calles al soltar el pin:
+    geocodeService.reverse().latlng(posicion, 15).run(function(error, resultado){
+      marker.bindPopup(resultado.address.LongLabel)
+    })
+    })
+
+})()
