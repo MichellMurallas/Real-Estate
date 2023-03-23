7d3237a217901ec77426b2a43db763388471a1e8
Miguel Angel <96792398+MichellMurallas@users.noreply.github.com>
2023-03-22 19:22:57 -0500

    Revert "Dev"

--- helpers/tokens.js
+++
@@ -1,8 +0,0 @@
-import jwt  from "jsonwebtoken";
-const generarId = () => Math.random().toString(32).substring(2) + Date.now().toString(32);
-const generarJWT = datos => jwt.sign({id: datos.id, nombre: datos.nombre }, process.env.JWT_SECRET, {expiresIn: "1d"})
-
-export {
-  generarId,
-  generarJWT
-}
