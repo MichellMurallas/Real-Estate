6e08e6e38590088460d6305c4b07787cb18a93c5
MichellMurallas <michellsms893@gmail.com>
2023-03-30 15:29:15 -0500

    import prices and categories

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,3 +1,5 @@
+import Precio from "../models/Precio.js"
+import Categoria from "../models/Categoria.js"
 
 const admin = (req, res) =>{
   res.render("propiedades/admin", {
@@ -7,11 +9,18 @@ const admin = (req, res) =>{
 }
 
 //Formulario para crear propiedad:
+const crear = async(req, res) =>{
+  //Consultar modelo de precio y categorias
+  const [categorias, precios] = await Promise.all([
+    Categoria.findAll(),
+    Precio.findAll()
+  ])
 
-const crear = (req, res) =>{
   res.render("propiedades/crear", {
     pagina: "Crear Propiedad",
-    barra: true
+    barra: true,
+    categorias,
+    precios
   })
 }
 
