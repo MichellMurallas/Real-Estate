3312c5b63704e1eb9029b422165c419d8a84e6d4
MichellMurallas <michellsms893@gmail.com>
2023-03-31 13:07:20 -0500

    validation1

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,5 +1,7 @@
+import { validationResult } from "express-validator"
 import Precio from "../models/Precio.js"
 import Categoria from "../models/Categoria.js"
+//import { promises } from "nodemailer/lib/xoauth2/index.js"
 
 const admin = (req, res) =>{
   res.render("propiedades/admin", {
@@ -19,12 +21,36 @@ const crear = async(req, res) =>{
   res.render("propiedades/crear", {
     pagina: "Crear Propiedad",
     barra: true,
+    csrfToken: req.csrfToken(),
     categorias,
     precios
   })
 }
 
+const guardar = async(req, res) =>{
+  //Validacion:
+  let resultado = validationResult(req)
+
+  if(!resultado.isEmpty()){
+    //Consultar modelo de precio y categorias
+    const [categorias, precios] = await Promise.all([
+      Categoria.findAll(),
+      Precio.findAll()
+    ])
+
+    return   res.render("propiedades/crear", {
+      pagina: "Crear Propiedad",
+      barra: true,
+      csrfToken: req.csrfToken(),
+      categorias,
+      precios,
+      errores: resultado.array()
+    })
+  }
+}
+
 export {
   crear,
-  admin
+  admin,
+  guardar
 }
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,9 +1,14 @@
 import  express from "express";
-import { admin, crear } from "../controllers/propiedadController.js"
+import { body } from "express-validator";
+import { admin, crear, guardar } from "../controllers/propiedadController.js"
 
 const router = express.Router()
 
 router.get("/mis-propiedades", admin)
 router.get("/propiedades/crear", crear)
+router.post("/propiedades/crear", 
+  body("titulo").notEmpty().withMessage("El titulo del anuncio es Obligatorio"),
+  guardar
+)
 
 export default router
